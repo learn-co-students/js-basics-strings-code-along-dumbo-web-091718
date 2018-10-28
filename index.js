60542f6154e9b722a6d4d2ebd0a24c3070470b99
github username <github email address>
2018-10-27 21:16:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "Raj";
+
+var welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+var excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
