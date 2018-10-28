c43da3e0673174cd25b18fd51fc7d464a0a4f89b
Bijon Miles <bijonmiles01@gmail.com>
2018-10-28 15:26:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+
+const currentUser = "string";
+
+let welcomeMessage = 'Welcome to Flatbook, ';
+welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ';
+excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+
+let shortGreeting = 'Welcome, ';
+shortGreeting = 'Welcome, ' + currentUser.substring(0,1) + '!';
